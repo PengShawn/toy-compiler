cfb01c90268ab20440df4ec4c7c88813d70ee9ee
肃清 <3187879878@qq.com>
2020-12-28 23:54:19 +0800

    初始化建立编辑器

--- src/main.js
+++ src/main.js
@@ -1,12 +1,18 @@
 import Vue from 'vue'
+import VueCodemirror from "vue-codemirror/src";
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import './plugins/element.js'
+import less from 'less'
+import './assets/css/global.css'   //导入全局样式表
+import 'codemirror/lib/codemirror.css'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+Vue.use(VueCodemirror,);
 
 new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,104 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <el-container class="home-container">
+      <el-header>
+        <div>
+          <span>Toy Compiler</span>
+          <span style="letter-spacing:1px; ">简单编译器</span>
+        </div>
+      </el-header>
+      <el-main>
+        <div>
+          Main
+          <codemirror v-model="code" :options="cmOption" />
+        </div>
+      </el-main>
+    </el-container>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+  import dedent from 'dedent'
+  import CodeMirror from 'codemirror'
+  import { codemirror } from 'vue-codemirror'
 
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  // theme css
+  import 'codemirror/theme/solarized.css'
+
+  // define custom mode
+  CodeMirror.defineMode('toy-mode', () => {
+    return {
+      token(stream, state) {
+        //console.log('测试打印',stream);
+        if (stream.match('int')) {
+          return 'keyword'
+        } else if (stream.match('real')) {
+          return 'keyword'
+        } else if (stream.match('if')) {
+          return 'keyword'
+        } else if (stream.match('then')) {
+          return 'keyword'
+        } else if (stream.match('else')) {
+          return 'keyword'
+        } else if (stream.match('while')) {
+          return 'keyword'
+        } else if (stream.match("^[1-9]$")) {
+          return 'number'
+        } else if (stream.match("12")) {
+          return 'number'
+        } else {
+          stream.next();
+          return null
+        }
+      }
+    }
+  })
+  export default {
+    name: 'Home',
+    components: {
+      codemirror
+    },
+    data() {
+      return {
+        code: `int bbb = 222;\nreal ccc = 111; if(bbb > 0 )\neee fff ggg`,
+        cmOption: {
+          tabSize: 4,
+          styleActiveLine: true,
+          lineNumbers: true,
+          line: true,
+          mode: 'toy-mode',
+          theme: 'solarized light'
+        }
+      }
+    }
   }
-}
 </script>
+
+<style lang="less" scoped>
+  .home-container {
+    height: 100%;
+  }
+
+  .el-header {
+    background-color: #96b97d;
+    display: flex;
+    justify-content: space-between;
+    padding-left: 0;
+    align-items: center;
+    color: #fff;
+    font-size: 20px;
+    > div {
+      display: flex;
+      align-items: center;
+      > span {
+        margin-left: 30px;
+        font-weight: 700;
+      }
+    }
+  }
+
+  .el-main {
+    background-color: #eaedf1;
+    height: 100%;
+  }
+</style>
