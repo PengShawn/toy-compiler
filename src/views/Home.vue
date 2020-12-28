e5db917bc04d2f6a0ff7c42bf1a58b88ffe30f44
肃清 <3187879878@qq.com>
2020-12-29 01:28:29 +0800

    使用codemirror实现代码高亮

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,13 +4,24 @@
       <el-header>
         <div>
           <span>Toy Compiler</span>
-          <span style="letter-spacing:1px; ">简单编译器</span>
+          <span style="letter-spacing:1px; ">在线编译器</span>
         </div>
       </el-header>
       <el-main>
         <div>
-          Main
-          <codemirror v-model="code" :options="cmOption" />
+          <el-card>
+            <div class="top-button">
+              <el-button type="success" icon="el-icon-s-promotion" size="small" >点击运行</el-button>
+              <el-switch
+                      style="margin-left: 20px"
+                      v-model="lightTheme"
+                      active-color="#5cb85c"
+                      active-text="暗黑模式"
+                      @change="changeTheme">
+              </el-switch>
+            </div>
+            <codemirror v-model="code" :options="cmOption" class="code-txt"/>
+          </el-card>
         </div>
       </el-main>
     </el-container>
@@ -25,25 +36,51 @@
   // theme css
   import 'codemirror/theme/solarized.css'
 
+
+  let keywords = ["int", "real", "if", "then", "else", "while"];
+  let operators = ["+", "-", "*", "/", "=", "==", "<","<=",">",">=","!="];
+  function arrayContains(needle, arrhaystack) {
+    return (arrhaystack.indexOf(needle) > -1);
+  }
   // define custom mode
   CodeMirror.defineMode('toy-mode', () => {
     return {
       token(stream, state) {
-        //console.log('测试打印',stream);
-        if (stream.match('int')) {
-          return 'keyword'
-        } else if (stream.match('real')) {
-          return 'keyword'
-        } else if (stream.match('if')) {
-          return 'keyword'
-        } else if (stream.match('then')) {
-          return 'keyword'
-        } else if (stream.match('else')) {
-          return 'keyword'
-        } else if (stream.match('while')) {
-          return 'keyword'
-        } else if (stream.match("^[1-9]$")) {
+        if (stream.match("=")) {
+          return 'operator'
+        } else if (stream.match("+")) {
+          return 'operator'
+        } else if (stream.match("-")) {
+          return 'operator'
+        } else if (stream.match("*")) {
+          return 'operator'
+        } else if (stream.match("/")) {
+          return 'operator'
+        } else if (stream.match(">")) {
+          return 'operator'
+        } else if (stream.match("<")) {
+          return 'operator'
+        } else if (stream.match("!")) {
+          return 'operator'
+        } else if (stream.match(";")) {
           return 'number'
+        } else if (stream.match(",")) {
+          return 'number'
+        } else if (stream.match("{")) {
+          return 'number'
+        } else if (stream.match("}")) {
+          return 'number'
+        } else if (stream.match("(")) {
+          return 'number'
+        } else if (stream.match(")")) {
+          return 'number'
+        }
+        stream.eatWhile(/\w/);
+        //console.log('测试打印',stream);
+        if(arrayContains(stream.current(), keywords) ){
+          return 'keyword'
+        } else if (arrayContains(stream.current(), operators)) {
+          return 'keyword'
         } else if (stream.match("12")) {
           return 'number'
         } else {
@@ -60,7 +97,7 @@
     },
     data() {
       return {
-        code: `int bbb = 222;\nreal ccc = 111; if(bbb > 0 )\neee fff ggg`,
+        code: `int bbb = 222; ,\nreal ccc = 111; if(bbb > 0 )\neee fff ggg`,
         cmOption: {
           tabSize: 4,
           styleActiveLine: true,
@@ -68,7 +105,14 @@
           line: true,
           mode: 'toy-mode',
           theme: 'solarized light'
-        }
+        },
+        lightTheme: false
+      }
+    },
+    methods:{
+      changeTheme() {
+        if(this.lightTheme) this.cmOption.theme = 'solarized dark';
+        else this.cmOption.theme = 'solarized light'
       }
     }
   }
@@ -100,5 +144,12 @@
   .el-main {
     background-color: #eaedf1;
     height: 100%;
+  }
+
+  .top-button {
+    margin-bottom: 10px;
+  }
+  .code-txt {
+    width: 500px;
   }
 </style>
